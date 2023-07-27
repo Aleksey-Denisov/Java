17eec24858e9ab20d889a44e2a7a5d0d296df33c
Aleksey <lexsys2014@hotmail.com>
2023-07-27 20:54:28 +0300

    Add solution hw2

---
+++ JavaCore/Homework2/src/ru/gbhw/java/view/Main.java
@@ -0,0 +1,9 @@
+package ru.gbhw.java.view;
+
+import ru.gbhw.java.controller.DataAndFuncController;
+
+public class Main {
+    public static void main(String[] args) {
+        new DataAndFuncController();
+    }
+}
