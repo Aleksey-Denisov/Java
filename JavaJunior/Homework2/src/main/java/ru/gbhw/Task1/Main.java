4551bac602c772199be8f2f068826a2afd2c8223
Aleksey <lexsys2014@hotmail.com>
2023-12-03 21:32:43 +0300

    add hw2

--- JavaJunior/Homework2/src/main/java/ru/gbhw/Task1/Main.java
+++ JavaJunior/Homework2/src/main/java/ru/gbhw/Task1/Main.java
@@ -7,32 +7,42 @@ public class Main {
     public static void main(String[] args) {
         Animal[] animals = {new Cat("Барсик", 2), new Dog("Шарик", 3), new Cat("Пушок", 4)};
         for(Animal animal : animals) {
-            ArrayList<Field> allFields = new ArrayList<>();
-            ArrayList<Constructor<?>> allConstructors = new ArrayList<>();
-            ArrayList<Method> allMethods = new ArrayList<>();
             Class<?> animalClass = animal.getClass();
-            Collections.addAll(allFields, animalClass.getSuperclass().getDeclaredFields());
-            Collections.addAll(allFields, animalClass.getDeclaredFields());
             System.out.println("Информация о классе: " + animalClass.getSimpleName() + ", имеет следующие поля:");
-            //Выведем все поля класса
-            allFields.forEach(field -> {
-                field.setAccessible(true);
-                try {
-                    //Ради эксперимента добавлю в какое нибудь поле значение
-                    if(field.getName().equals("swims") && field.getType() == int.class)
-                        field.set(animal, 100);
-                    System.out.println("Поле класса: " + field.getName() + ", с значением: " + field.get(animal));
-                } catch (IllegalArgumentException | IllegalAccessException ignored) {
-                }
-            });
-            //Получим все конструкторы
-            Collections.addAll(allConstructors, animalClass.getSuperclass().getConstructors());
-            Collections.addAll(allConstructors, animalClass.getConstructors());
-            allConstructors.forEach(constructor -> {
-                System.out.println("Конструктор: " + constructor);
-            });
-            //Обработка методов классов
-            Collections.addAll(allMethods, animalClass.getSuperclass().getDeclaredMethods());
+            showAllFields(animal, animalClass);
+            showAllConstructors(animalClass);
+            showAllMethods(animal, animalClass);
+        }
+    }
+    //Функция получения всех полей в классе
+    private static void showAllFields(Animal animal, Class<?> animalClass) {
+        ArrayList<Field> allFields = new ArrayList<>();
+        Collections.addAll(allFields, animalClass.getSuperclass().getDeclaredFields());
+        Collections.addAll(allFields, animalClass.getDeclaredFields());
+        allFields.forEach(field -> {
+            field.setAccessible(true);
+            try {
+                //Ради эксперимента добавлю в какое нибудь поле значение
+                if(field.getName().equals("swims") && field.getType() == int.class)
+                    field.set(animal, 100);
+                System.out.println("Поле класса: " + field.getName() + ", с значением: " + field.get(animal));
+            } catch (IllegalArgumentException | IllegalAccessException ignored) {
+            }
+        });
+    }
+    //Функция получения всех конструкторов в классе
+    private static void showAllConstructors(Class<?> animalClass) {
+        ArrayList<Constructor<?>> allConstructors = new ArrayList<>();
+        Collections.addAll(allConstructors, animalClass.getSuperclass().getConstructors());
+        Collections.addAll(allConstructors, animalClass.getConstructors());
+        allConstructors.forEach(constructor -> {
+            System.out.println("Конструктор: " + constructor);
+        });
+    }
+    //Функция получения всех методов в классе
+    private static void showAllMethods(Animal animal, Class<?> animalClass) {
+        ArrayList<Method> allMethods = new ArrayList<>();
+        Collections.addAll(allMethods, animalClass.getSuperclass().getDeclaredMethods());
             Collections.addAll(allMethods, animalClass.getDeclaredMethods());
             allMethods.forEach(method -> {
                 method.setAccessible(true);
@@ -52,9 +62,8 @@ public class Main {
                     System.out.println("Результат вызванной функции makeSound: " + method.invoke(animal));
             } catch (NoSuchMethodException | SecurityException | IllegalAccessException | InvocationTargetException ignored) {
             }
-        }
     }
-
+    //Функция получения параметров в методе
     private static String getParameters(Parameter[] parameters) {
         StringBuilder result = new StringBuilder();
         result.append("(");
